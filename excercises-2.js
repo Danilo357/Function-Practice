c5045f2d20becf41c3650e3456202e7662fa9107
Danilo Nikolic <nikolicdanilo1410@gmail.com>
2019-10-08 18:58:24 -0700

    functions with if and else

--- excercises-2.js
+++ excercises-2.js
@@ -3,7 +3,14 @@
 // arguments and returns the largest of them. Use the 
 // if-then-else construct available in JavaScript.
 // ---------------------
+function max(a,b){
+    if(a>b){
+        return (a)
+    }else{
+        return(b)
+    }
 
+}
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
 console.assert(max(-23, 12) === 12)
@@ -12,16 +19,43 @@ console.assert(max(-23, 12) === 12)
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
+function maxOfThree(a,b,c){
+ if(a>b && a>c){
+     return(a)
+ }else if(b>c){
+     return(b)
+ }else{
+     return(c)
+ }
+ 
+}
 
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
 console.assert(maxOfThree(-12,4,-5) === 4)
 
-// ---------------------
-// Write a function isVowel() that takes a character (i.e. a 
-// string of length 1) and returns true if it is a 
-// vowel, false otherwise.
-// ---------------------
+// // ---------------------
+// // Write a function isVowel() that takes a character (i.e. a 
+// // string of length 1) and returns true if it is a 
+// // vowel, false otherwise.
+// // ---------------------
+
+function isVowel(dot){
+    var vowels = "aeiouAEIOU";
+    if(typeof dot == "string")
+    {
+      if (vowels.includes(dot)){
+          return true
+
+      }else{
+          return false
+      }
+      }else{
+        return false
+      
+    }
+
+}
 
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
@@ -30,13 +64,28 @@ console.assert(isVowel("a") === true);
 console.assert(isVowel("E") === true);
 console.assert(isVowel("2") === false)
 
-// ---------------------
-// Write a function rovarspraket() that will translate 
-// a text into "Rovarspraket". That is, double every 
-// consonant and place an occurrence of "o" in between. 
-// For example, translate("this is fun") should return 
-// the string "tothohisos isos fofunon".
-// ---------------------
+// // ---------------------
+// // Write a function rovarspraket() that will translate 
+// // a text into "Rovarspraket". That is, double every 
+// // consonant and place an occurrence of "o" in between. 
+// // For example, translate("this is fun") should return 
+// // the string "tothohisos isos fofunon".
+// // ---------------------
+
+
+function rovarspraket(code) {
+
+let string = code.toString()
+
+let vowel=["aeiouAEIOU"]
+
+let new=["0123456789"]
+for(
+    i=0; i<string.length; i++){
+        var word=string.charAt(i)
+    }if (vowel.indexOf(word) !=-1  ||  new.indexof() !=-1
+)
+}
 
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
@@ -45,11 +94,24 @@ console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
 console.assert(rovarspraket(0) === "0")
 
 
-// ---------------------
-// Define a function reverse() that computes the reversal 
-// of a string. For example, reverse("jag testar") should 
-// return the string "ratset gaj".
-// ---------------------
+// // ---------------------
+// // Define a function reverse() that computes the reversal 
+// // of a string. For example, reverse("jag testar") should 
+// // return the string "ratset gaj".
+// // ---------------------
+
+function reverse(str){
+    var arr = str.split('')
+    // console.log(arr.length)
+    var newArr = []
+    for(let i=arr.length -1; i>=0; i--){
+
+        newArr.push(arr[i])
+    }
+
+    return newArr.join('')
+    // return str.split('').reverse().join('')
+};
 
 
 console.assert(reverse("books") === "skoob")
